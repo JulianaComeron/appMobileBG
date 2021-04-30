fec50c763049eadde3f3cf5babc82f9551a5f3ca
Juliana <juliana_s_castro@hotmail.com>
2021-04-29 22:57:20 -0300

    Alterações das cores, gerada a tela de splash e implementação da tela login com milesegundos

--- app/src/main/java/com/example/appbg/LoginActivity.java
+++ app/src/main/java/com/example/appbg/LoginActivity.java
@@ -2,13 +2,25 @@ package com.example.appbg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button Enviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Enviar = findViewById(R.id.Enviar);
+
+        Enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaMenu= new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(telaMenu);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/appbg/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.appbg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int TEMPO = 2200; //EM MILLESEGUNDOS
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        inicializarTelaLogin();
+    }
+
+    private void inicializarTelaLogin(){
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                Intent telaLogin = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(telaLogin);
+            }
+        },TEMPO);
+
+    }
+}
