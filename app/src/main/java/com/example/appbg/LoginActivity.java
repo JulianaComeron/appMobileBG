774d4f7979eee5e8094a2d5c4284e2a014748d58
Juliana <juliana_s_castro@hotmail.com>
2021-05-03 22:34:00 -0300

    Tela de Login

--- app/src/main/java/com/example/appbg/LoginActivity.java
+++ app/src/main/java/com/example/appbg/LoginActivity.java
@@ -6,21 +6,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    Button Enviar;
+    EditText editTextLogin;
+    EditText editTextSenha;
+    Button buttonEnviar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Enviar = findViewById(R.id.Enviar);
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextSenha = findViewById(R.id.editTextSenha);
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+        abrirTelaMenu();
+    }
 
-        Enviar.setOnClickListener(new View.OnClickListener() {
+    private void abrirTelaMenu() {
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent telaMenu= new Intent(getApplicationContext(), MenuActivity.class);
-                startActivity(telaMenu);
+                //verificar se o usuário e a senha são iguais.
+                if (editTextLogin.getText().toString().equals("a") &&
+                        editTextSenha.getText().toString().equals("a")) {
+                    Toast.makeText(LoginActivity.this, "Bem-Vindo", Toast.LENGTH_SHORT).show();
+                    Intent telaMenu = new Intent(getApplicationContext(), MenuActivity.class);
+                    startActivity(telaMenu);
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Campos são diferentes", Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
+    }
+
+    private boolean verifcarCampoVazio(){
+        boolean vazio;
+        if(editTextLogin.getText().toString().isEmpty()){
+            Toast.makeText(this, "O campo login não pode ser vazio", Toast.LENGTH_SHORT).show();
+            vazio=false;
+        }
+        else if(editTextSenha.getText().toString().isEmpty()){
+            Toast.makeText(this, "O campo senhão não pode estar vazio", Toast.LENGTH_SHORT).show();
+            vazio = false;
+        }else{
+            vazio=true;
+        }
+        return vazio;
     }
 }
